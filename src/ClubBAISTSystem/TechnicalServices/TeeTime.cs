4fc95df78906189ce59c18f7d3b84e7c16a0768b
Mason <masonclarke35@gmail.com>
2019-11-14 19:57:20 -0700

    Initial Commit

---
+++ src/ClubBAISTSystem/TechnicalServices/TeeTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechnicalServices
+{
+    public class TeeTime
+    {
+        public DateTime Date { get; set; }
+        public int NumberOfGolfers { get; set; }
+        public string[] Golfers { get; set; }
+        public string Phone { get; set; }
+    }
+}
